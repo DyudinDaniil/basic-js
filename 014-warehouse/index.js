9c047822b1906c83e02f67c8c3fcc7d653571015
DyudinDaniil <dyudindaniil2002@gmail.com>
2024-02-27 10:55:14 +0300

    014 watehouse

---
+++ 014-warehouse/index.js
@@ -0,0 +1,51 @@
+const warehous = {
+    goods: [],
+    addGood: function(good) {
+        this.goods.push(good);
+    },
+    findGoodById: function(id) {
+        for (good of this.goods) {
+            if (good.id == id) {
+                return good;
+            }
+        }
+
+        return false;
+    },
+    getWeight: function() {
+        const arrOfWeight = this.goods.map(good => good?.weight?.kg);
+        return arrOfWeight.reduce((acc, sum) => {return acc += sum ? sum : 0}, 0);
+    }
+}
+
+const car = {
+    id: 1,
+    weight: {
+        kg: 1000
+    },
+    brand: 'Ford'
+}
+
+const chair = {
+    id: 2,
+    weight: {
+        kg: 4
+    }
+}
+
+const paper = {
+    id: 3,
+    color: 'red'
+}
+
+warehous.addGood(car);
+warehous.addGood(chair);
+warehous.addGood(paper);
+console.log(warehous.goods);
+
+console.log(warehous.findGoodById(1));
+console.log(warehous.findGoodById(2));
+console.log(warehous.findGoodById(3));
+console.log(warehous.findGoodById(4));
+
+console.log(warehous.getWeight());
